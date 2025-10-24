c19cf13581fbfe3257d2e790080ff5a6ce1beb35
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-10-24 10:47:29 +0100

    Add after_boot_rom_dmg() to initialize registers to their post-bootrom values.

--- sm83-interp/src/lib.rs
+++ sm83-interp/src/lib.rs
@@ -1,4 +1,4 @@
 pub mod cpu;
 pub mod opcodes;
 mod parameters;
-mod registers;
+pub mod registers;
--- sm83-interp/src/registers.rs
+++ sm83-interp/src/registers.rs
@@ -46,6 +46,34 @@ pub struct Registers {
 }
 
 impl Registers {
+    /// Initialize registers to the value they'd be just after executing the DMG boot rom.
+    /// See: <https://gbdev.io/pandocs/Power_Up_Sequence.html#cpu-registers>
+    #[must_use] 
+    pub fn after_boot_rom_dmg() -> Self {
+        Self {
+            af: AccumAndFlags::new()
+                .with_a(0x01)
+                .with_f(
+                    Flags::new()
+                        .with_z(true)
+                        .with_n(false)
+                        .with_h(true)
+                        .with_c(true)
+                ),
+            bc: Bc::new()
+                .with_b(0x00)
+                .with_c(0x13),
+            de: De::new()
+                .with_d(0x00)
+                .with_e(0xD8),
+            hl: Hl::new()
+                .with_h(0x01)
+                .with_l(0x4D),
+            pc: 0x0100,
+            sp: 0xFFFE,
+        }
+    }
+
     pub(crate) fn r16_mut(&mut self, r16: R16) -> &mut u16 {
         match r16 {
             R16::Bc => &mut self.bc.0,
