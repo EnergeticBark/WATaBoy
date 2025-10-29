4ec8bd14f4692615592071454413f2a019b1cb57
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-10-29 12:26:12 +0000

    Add integration tests for Blargg's 06-08. Fix background tile map being chunked by the wrong amount of bytes per line.

--- sm83-interp/tests/common/mod.rs
+++ sm83-interp/tests/common/mod.rs
@@ -2,7 +2,7 @@ use sm83_interp::cpu::Cpu;
 use sm83_interp::registers::Registers;
 
 pub fn read_ascii_from_tile_map(cpu: &Cpu) -> Vec<String> {
-    let (lines, _) = cpu.memory.buffer[0x9800..0x9C00].as_chunks::<48>();
+    let (lines, _) = cpu.memory.buffer[0x9800..0x9C00].as_chunks::<32>();
     lines.iter()
         .map(|line| str::from_utf8(line))
         .map(|result| result.unwrap().to_owned())
--- sm83-interp/tests/cpu_instrs.rs
+++ sm83-interp/tests/cpu_instrs.rs
@@ -38,38 +38,72 @@ const OP_RP_05: BlarggTest = BlarggTest {
     rom: include_bytes!("./roms/05-op rp.gb"),
     final_pc: 0xCB31,
 };
+const LD_R_R_06: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/06-ld r,r.gb"),
+    final_pc: 0xCC5F,
+};
+const JR_JP_CALL_RET_RST_07: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/07-jr,jp,call,ret,rst.gb"),
+    final_pc: 0xCBB0,
+};
+const MISC_INSTRS_08: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/08-misc instrs.gb"),
+    final_pc: 0xCB91,
+};
 
 #[test]
 fn test_01_special() {
     let mut cpu = Cpu::default();
     let lines = run_blargg_test(&mut cpu, &SPECIAL_01);
-    assert!(lines[2].starts_with("Passed"));
+    assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_02_interrupts() {
     let mut cpu = Cpu::default();
     let lines = run_blargg_test(&mut cpu, &INTERRUPTS_02);
-    assert!(lines[2].starts_with("Passed"));
+    assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_03_op_sp_hl() {
     let mut cpu = Cpu::default();
     let lines = run_blargg_test(&mut cpu, &OP_SP_HL_03);
-    assert!(lines[2].starts_with("Passed"));
+    assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_04_op_r_imm() {
     let mut cpu = Cpu::default();
     let lines = run_blargg_test(&mut cpu, &OP_R_IMM_04);
-    assert!(lines[2].starts_with("Passed"));
+    assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_05_op_rp() {
     let mut cpu = Cpu::default();
     let lines = run_blargg_test(&mut cpu, &OP_RP_05);
-    assert!(lines[2].starts_with("Passed"));
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_06_ld_r_r() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &LD_R_R_06);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_07_jr_jp_call_ret_rst() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &JR_JP_CALL_RET_RST_07);
+    // This test's name is so long that "Passed" ends up on line 4 :)
+    assert!(lines[4].starts_with("Passed"));
+}
+
+#[test]
+fn test_08_misc_instrs() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &MISC_INSTRS_08);
+    assert!(lines[3].starts_with("Passed"));
 }
