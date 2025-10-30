cf3b4041fa3a7df7c11cbb6e9ec27bfbab1a3e21
Seth Humphries <33643461+EnergeticBark@users.noreply.github.com>
2025-10-30 09:49:15 +0000

    blarggs-tests (#5)
    * Add integration test for Blargg's 01-special.gb.
    
    * Add integration test for Blargg's 02-interrupts.gb.
    
    * Add integration test for Blargg's 02-interrupts.gb.
    
    * Add integration test for Blargg's 03-op sp,hl.gb.
    Consolidate cpu_instrs tests into a single file.
    
    * Add integration tests for Blargg's 04-op r,imm.gb and 05-op rp.gb.
    
    * Add integration tests for Blargg's 06-08.
    Fix background tile map being chunked by the wrong amount of bytes per line.
    
    * Add integration tests for Blargg's 09-11.

---
+++ sm83-interp/tests/common/mod.rs
@@ -0,0 +1,22 @@
+use sm83_interp::cpu::Cpu;
+use sm83_interp::registers::Registers;
+
+pub fn read_ascii_from_tile_map(cpu: &Cpu) -> Vec<String> {
+    let (lines, _) = cpu.memory.buffer[0x9800..0x9C00].as_chunks::<32>();
+    lines.iter()
+        .map(|line| str::from_utf8(line))
+        .map(|result| result.unwrap().to_owned())
+        .collect()
+}
+
+pub fn load_test_rom(cpu: &mut Cpu, test_rom: &[u8]) {
+    cpu.memory[0..0x8000].copy_from_slice(&test_rom[0..0x8000]);
+    cpu.registers = Registers::after_boot_rom_dmg();
+}
+
+pub fn execute_until(cpu: &mut Cpu, final_pc: u16) {
+    while cpu.registers.pc != final_pc {
+        cpu.execute().unwrap();
+        cpu.handle_interrupts();
+    }
+}
---
+++ sm83-interp/tests/cpu_instrs.rs
@@ -0,0 +1,142 @@
+mod common;
+
+use sm83_interp::cpu::Cpu;
+
+struct BlarggTest {
+    rom: &'static [u8],
+    /* Blargg's test roms will put themselves in an infinite loop after passing or failing.
+      We stop and run our assertions once the program counter reaches this looping instruction.
+      [We probably want to implement a timeout at some point.]
+    */
+    final_pc: u16,
+}
+
+fn run_blargg_test(cpu: &mut Cpu, test: &BlarggTest) -> Vec<String> {
+    common::load_test_rom(cpu, test.rom);
+    common::execute_until(cpu, test.final_pc);
+
+    common::read_ascii_from_tile_map(cpu)
+}
+
+const SPECIAL_01: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/01-special.gb"),
+    final_pc: 0xC7D2,
+};
+const INTERRUPTS_02: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/02-interrupts.gb"),
+    final_pc: 0xC7F4,
+};
+const OP_SP_HL_03: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/03-op sp,hl.gb"),
+    final_pc: 0xCB44,
+};
+const OP_R_IMM_04: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/04-op r,imm.gb"),
+    final_pc: 0xCB35,
+};
+const OP_RP_05: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/05-op rp.gb"),
+    final_pc: 0xCB31,
+};
+const LD_R_R_06: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/06-ld r,r.gb"),
+    final_pc: 0xCC5F,
+};
+const JR_JP_CALL_RET_RST_07: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/07-jr,jp,call,ret,rst.gb"),
+    final_pc: 0xCBB0,
+};
+const MISC_INSTRS_08: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/08-misc instrs.gb"),
+    final_pc: 0xCB91,
+};
+const OP_R_R_09: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/09-op r,r.gb"),
+    final_pc: 0xCE67,
+};
+const BIT_OPS_10: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/10-bit ops.gb"),
+    final_pc: 0xCF58,
+};
+const OP_A_HL_11: BlarggTest = BlarggTest {
+    rom: include_bytes!("./roms/11-op a,(hl).gb"),
+    final_pc: 0xCC62,
+};
+
+#[test]
+fn test_01_special() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &SPECIAL_01);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_02_interrupts() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &INTERRUPTS_02);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_03_op_sp_hl() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &OP_SP_HL_03);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_04_op_r_imm() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &OP_R_IMM_04);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_05_op_rp() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &OP_RP_05);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_06_ld_r_r() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &LD_R_R_06);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_07_jr_jp_call_ret_rst() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &JR_JP_CALL_RET_RST_07);
+    // This test's name is so long that "Passed" ends up on line 4 :)
+    assert!(lines[4].starts_with("Passed"));
+}
+
+#[test]
+fn test_08_misc_instrs() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &MISC_INSTRS_08);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_09_op_r_r() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &OP_R_R_09);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_10_bit_ops() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &BIT_OPS_10);
+    assert!(lines[3].starts_with("Passed"));
+}
+
+#[test]
+fn test_11_op_a_hl() {
+    let mut cpu = Cpu::default();
+    let lines = run_blargg_test(&mut cpu, &OP_A_HL_11);
+    assert!(lines[3].starts_with("Passed"));
+}
