b62de29c33f86850c63594d750edb4feba7071bb
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2025-11-10 13:59:18 +0000

    Write unit tests for varying scroll+bg+window combinations.

--- ppu/src/ppu.rs
+++ ppu/src/ppu.rs
@@ -5,6 +5,8 @@ const SCANLINES_PER_FRAME: usize = 154;
 const DOTS_PER_SCANLINE: usize = 456;
 const DOTS_PER_FRAME: usize = DOTS_PER_SCANLINE * SCANLINES_PER_FRAME;
 
+const OAM_SCAN_DOTS: usize = 80;
+
 const SCX: usize = 0xFF43;
 
 const WY: usize = 0xFF4A;
@@ -42,7 +44,7 @@ impl Ppu {
         match self.mode {
             PpuMode::OamScan => {
                 self.dot_counter += 1;
-                if self.dot_counter % DOTS_PER_SCANLINE >= 80 {
+                if self.dot_counter % DOTS_PER_SCANLINE >= OAM_SCAN_DOTS {
                     self.pixels_to_drop = memory[SCX] & 7;
                     self.mode = PpuMode::Drawing;
                 }
@@ -51,7 +53,7 @@ impl Ppu {
                 self.dot_counter += 1;
 
                 self.fetcher.tick(memory, self.ly() as u8, self.window_y);
-                println!("Dot: {}, X: {}, FIFO: {}", (self.dot_counter % DOTS_PER_SCANLINE) - 80, self.x, self.fetcher.bg_fifo.len());
+                println!("Dot: {}, X: {}, FIFO: {}", (self.dot_counter % DOTS_PER_SCANLINE) - OAM_SCAN_DOTS, self.x, self.fetcher.bg_fifo.len());
 
                 if let Some(pixel) = self.fetcher.shift_out() {
                     if self.pixels_to_drop > 0 {
@@ -80,7 +82,7 @@ impl Ppu {
                 }
 
                 if self.x >= 160 {
-                    println!("Drew for {} dots", (self.dot_counter % DOTS_PER_SCANLINE) - 80);
+                    println!("Drew for {} dots", (self.dot_counter % DOTS_PER_SCANLINE) - OAM_SCAN_DOTS);
                     self.x = 0;
                     self.fetcher = PixelFetcher::default();
                     self.mode = PpuMode::HBlank;
@@ -119,5 +121,100 @@ impl Default for Ppu {
             fetcher: PixelFetcher::default(),
             funny_buffer_test: vec![0; 160 * 144],
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Assert that the minimum Mode 3 length (172) with:
+    // - unscrolled background tiles (0)
+    // - no window (0)
+    // - no objects (0)
+    // is 172 dots.
+    // See: https://gbdev.io/pandocs/Rendering.html#mode-3-length
+    #[test]
+    fn test_minimum_bg_mode_3_dots() {
+        let mut ppu = Ppu::default();
+        let memory = [0; 0x10000];
+
+        while !matches!(ppu.mode, PpuMode::HBlank) {
+            ppu.tick(&memory);
+        }
+
+        let mode_3_dots = ppu.dot_counter - OAM_SCAN_DOTS;
+        assert_eq!(mode_3_dots, 172);
+    }
+
+    // Assert that the minimum Mode 3 length (172) with:
+    // - scrolled background tiles (SCX % 8 = 7)
+    // - no window (0)
+    // - no objects (0)
+    // is 179 dots.
+    // See: https://gbdev.io/pandocs/Rendering.html#mode-3-length
+    #[test]
+    fn test_scrolled_bg_mode_3_dots() {
+        let mut ppu = Ppu::default();
+        let mut memory = [0; 0x10000];
+        memory[SCX] = 7;
+
+        while !matches!(ppu.mode, PpuMode::HBlank) {
+            ppu.tick(&memory);
+        }
+
+        let mode_3_dots = ppu.dot_counter - OAM_SCAN_DOTS;
+        assert_eq!(mode_3_dots, 179);
+    }
+
+    // Assert that the minimum Mode 3 length (172) with:
+    // - unscrolled background tiles (0)
+    // - a window (6)
+    // - no objects (0)
+    // is 178 dots.
+    // See: https://gbdev.io/pandocs/Rendering.html#mode-3-length
+    #[test]
+    fn test_minimum_bg_window_mode_3_dots() {
+        let mut ppu = Ppu::default();
+        let mut memory = [0; 0x10000];
+
+        // Enable the window.
+        const LCDC: usize = 0xFF40;
+        memory[LCDC] |= 0b0010_0000;
+        // Scroll it to x=50px
+        memory[WX] = 50 + 7;
+
+        while !matches!(ppu.mode, PpuMode::HBlank) {
+            ppu.tick(&memory);
+        }
+
+        let mode_3_dots = ppu.dot_counter - OAM_SCAN_DOTS;
+        assert_eq!(mode_3_dots, 178);
+    }
+
+    // Assert that the minimum Mode 3 length (172) with:
+    // - scrolled background tiles (SCX % 8 = 7)
+    // - a window (6)
+    // - no objects (0)
+    // is 185 dots.
+    // See: https://gbdev.io/pandocs/Rendering.html#mode-3-length
+    #[test]
+    fn test_scrolled_bg_window_mode_3_dots() {
+        let mut ppu = Ppu::default();
+        let mut memory = [0; 0x10000];
+        memory[SCX] = 7;
+
+        // Enable the window.
+        const LCDC: usize = 0xFF40;
+        memory[LCDC] |= 0b0010_0000;
+        // Scroll it to x=50px
+        memory[WX] = 50 + 7;
+
+        while !matches!(ppu.mode, PpuMode::HBlank) {
+            ppu.tick(&memory);
+        }
+
+        let mode_3_dots = ppu.dot_counter - OAM_SCAN_DOTS;
+        assert_eq!(mode_3_dots, 185);
     }
 }
