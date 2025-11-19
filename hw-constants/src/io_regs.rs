6a7032675e92d04d558c48be1613ea30d45b0822
Seth Humphries <33643461+EnergeticBark@users.noreply.github.com>
2025-11-19 15:19:56 +0000

    Add LY to io_regs. Fixed "unused function" warnings by publicly importing common test helpers.

--- hw-constants/src/io_regs.rs
+++ hw-constants/src/io_regs.rs
@@ -22,5 +22,6 @@ pub const LCDC: u16 = 0xFF40;
 pub const STAT: u16 = 0xFF41;
 pub const SCY: u16 = 0xFF42;
 pub const SCX: u16 = 0xFF43;
+pub const LY: u16 = 0xFF44;
 pub const WY: u16 = 0xFF4A;
 pub const WX: u16 = 0xFF4B;
