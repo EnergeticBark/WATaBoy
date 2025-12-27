ed7a31579fb3491c39e0599feafe896210b881b8
Seth Humphries <33643461+EnergeticBark@users.noreply.github.com>
2025-12-27 13:57:36 +0000

    Add screen and tilemaps constants. Add a sensible .gitignore.

--- hw-constants/src/lib.rs
+++ hw-constants/src/lib.rs
@@ -3,3 +3,10 @@ pub mod io_regs;
 pub const OAM: u16 = 0xFE00;
 
 pub const IE: u16 = 0xFFFF;
+
+pub const SCREEN_WIDTH: usize = 160;
+pub const SCREEN_HEIGHT: usize = 144;
+
+pub const TILE_MAP_SIZE: usize = 256;
+
+pub const TILE_SIZE: usize = 8;
