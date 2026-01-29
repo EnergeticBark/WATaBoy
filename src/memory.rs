fa8ef3899f4c0dd3dc65bf3582a4566e54b55e1a
Seth Humphries <33643461+EnergeticBark@users.noreply.github.com>
2026-01-29 13:01:58 +0000

    Set the font to monospace once for the whole memory viewer.

--- src/memory.rs
+++ src/memory.rs
@@ -1,14 +1,15 @@
 use eframe::epaint::Color32;
-use egui::{RichText, Ui};
+use egui::{RichText, TextStyle, Ui};
 use sm83_interp::cpu::Cpu;
 
 pub fn draw_memory_table(ui: &mut Ui, dmg_state: &Cpu) {
+    ui.style_mut().override_text_style = Some(TextStyle::Monospace);
     ui.horizontal(|ui| {
-        ui.monospace("    ");
+        ui.label("    ");
 
         for column_number in 0..16 {
             let column_label = format!("{column_number:02X}");
-            ui.monospace(column_label);
+            ui.label(column_label);
         }
     });
 
@@ -21,20 +22,16 @@ fn draw_memory_body(ui: &mut Ui, dmg_state: &Cpu) {
             ui.horizontal(|ui| {
                 let row_index = row as u16 * 16;
                 let row_label = format!("{:03X}0", row_index / 16);
-                ui.monospace(row_label);
+                ui.label(row_label);
 
                 for i in 0..16 {
                     let formatted_row = format!("{:02X}", dmg_state.memory[row_index + i]);
 
-                    let label = RichText::from(formatted_row).strong();
-
-                    let label = if row_index + i == dmg_state.registers.pc {
-                        label.color(Color32::RED)
+                    if row_index + i == dmg_state.registers.pc {
+                        ui.strong(RichText::from(formatted_row).color(Color32::RED));
                     } else {
-                        label
-                    };
-
-                    ui.monospace(label);
+                        ui.strong(formatted_row);
+                    }
                 }
             });
         }
