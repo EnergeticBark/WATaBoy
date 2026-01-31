34d740ddfeb17ea41e1a94f1bf66907015df2734
Seth Humphries <33643461+EnergeticBark@users.noreply.github.com>
2026-01-31 15:00:00 +0000

    Add integration test for Mooneye's boot_regs-dmgABC.gb.

--- sm83-interp/tests/mooneye.rs
+++ sm83-interp/tests/mooneye.rs
@@ -11,3 +11,11 @@ fn test_boot_hwio_dmg_abc_mgb() {
 
     assert_eq!(bcdehl, FIBONACCI);
 }
+
+#[test]
+fn test_boot_regs_dmg_abc() {
+    let mut cpu = Cpu::post_boot_dmg();
+    let bcdehl = run_mooneye_test(&mut cpu, include_bytes!("roms/mooneye/boot_regs-dmgABC.gb"));
+
+    assert_eq!(bcdehl, FIBONACCI);
+}
