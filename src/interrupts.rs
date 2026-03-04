340e5901ecc800c8274637d502ec27482823e17a
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-03-04 14:42:31 +0000

    Show STAT values in the interrupts view.

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,11 +1,16 @@
-use egui::Checkbox;
+use egui::{Checkbox, Label, TextWrapMode};
 use egui_extras::{Column, TableBody, TableBuilder};
+use hw_constants::io_regs::STAT;
 use hw_constants::io_regs::{IE, IF};
 use sm83_interp::cpu::Cpu;
 use sm83_interp::cpu::InterruptBits;
+use sm83_interp::ppu::{LcdStatus, StatMode};
 
 pub fn show(ui: &mut egui::Ui, dmg_state: &Cpu) {
+    let name = "IE and IF";
+    ui.heading(name);
     TableBuilder::new(ui)
+        .id_salt(name)
         .striped(true)
         .column(Column::auto())
         .column(Column::auto())
@@ -22,11 +27,32 @@ pub fn show(ui: &mut egui::Ui, dmg_state: &Cpu) {
             });
         })
         .body(|body| {
-            draw_interrupts_body(body, dmg_state);
+            draw_ie_and_if_body(body, dmg_state);
+        });
+
+    ui.separator();
+
+    let name = "STAT";
+    ui.heading(name);
+    TableBuilder::new(ui)
+        .id_salt(name)
+        .striped(true)
+        .column(Column::auto())
+        .column(Column::remainder())
+        .header(18.0, |mut header| {
+            header.col(|ui| {
+                ui.label("Status");
+            });
+            header.col(|ui| {
+                ui.label("Value");
+            });
+        })
+        .body(|mut body| {
+            draw_stat_body(&mut body, dmg_state);
         });
 }
 
-fn draw_interrupts_body(body: TableBody<'_>, dmg_state: &Cpu) {
+fn draw_ie_and_if_body(body: TableBody<'_>, dmg_state: &Cpu) {
     let intr_enable = InterruptBits::from_bits(dmg_state.memory.buffer[IE as usize]);
     let intr_flag = InterruptBits::from_bits(dmg_state.memory.buffer[IF as usize]);
 
@@ -54,6 +80,77 @@ fn draw_interrupts_body(body: TableBody<'_>, dmg_state: &Cpu) {
         row.col(|ui| {
             let mut flagged: bool = flagged;
             ui.add_enabled(false, Checkbox::new(&mut flagged, ""));
+        });
+    });
+}
+
+fn draw_stat_body(body: &mut TableBody<'_>, dmg_state: &Cpu) {
+    let height = 18.0;
+    let stat = LcdStatus::from_bits(dmg_state.memory.buffer[STAT as usize]);
+
+    body.row(height, |mut row| {
+        row.col(|ui| {
+            ui.label("PPU Mode");
+        });
+        let stat_mode_name = match stat.mode() {
+            StatMode::HBlank => "0 (HBlank)",
+            StatMode::VBlank => "1 (VBlank)",
+            StatMode::OamScan => "2 (OAM Scan)",
+            StatMode::Drawing => "3 (Drawing)",
+        };
+        row.col(|ui| {
+            ui.label(stat_mode_name);
+        });
+    });
+
+    body.row(height, |mut row| {
+        row.col(|ui| {
+            ui.label("LYC == LY");
+        });
+        row.col(|ui| {
+            let mut checked: bool = stat.coincidence();
+            ui.add_enabled(false, Checkbox::new(&mut checked, ""));
+        });
+    });
+
+    body.row(height, |mut row| {
+        row.col(|ui| {
+            // Prevent this label from wrapping so the column it's in gets extended.
+            ui.add(Label::new("Mode 0 Int Select").wrap_mode(TextWrapMode::Extend));
+        });
+        row.col(|ui| {
+            let mut checked: bool = stat.mode0_int_select();
+            ui.add_enabled(false, Checkbox::new(&mut checked, ""));
+        });
+    });
+
+    body.row(height, |mut row| {
+        row.col(|ui| {
+            ui.label("Mode 1 Int Select");
+        });
+        row.col(|ui| {
+            let mut checked: bool = stat.mode1_int_select();
+            ui.add_enabled(false, Checkbox::new(&mut checked, ""));
+        });
+    });
+
+    body.row(height, |mut row| {
+        row.col(|ui| {
+            ui.label("Mode 2 Int Select");
+        });
+        row.col(|ui| {
+            let mut checked: bool = stat.mode2_int_select();
+            ui.add_enabled(false, Checkbox::new(&mut checked, ""));
+        });
+    });
+
+    body.row(height, |mut row| {
+        row.col(|ui| {
+            ui.label("LYC Int Select");
+        });
+        row.col(|ui| {
+            let mut checked: bool = stat.lyc_int_select();
+            ui.add_enabled(false, Checkbox::new(&mut checked, ""));
         });
     });
 }
