ed5e07430a817a93f88d469c7e1923e7dd37e3f3
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-03-06 14:34:57 +0000

    Import registers from io_regs.

--- sm83-interp/src/bus.rs
+++ sm83-interp/src/bus.rs
@@ -4,14 +4,17 @@ use crate::mbc::Mbc;
 use crate::ppu::Ppu;
 use crate::timers::Timers;
 
-use hw_constants::io_regs::{BGP, LCDC, LY, LYC, OBP0, OBP1, SCX, SCY, STAT, WX, WY};
-use hw_constants::{OAM_END, OAM_START, PostBoot, VRAM_END, VRAM_START, io_regs};
+use hw_constants::io_regs::{
+    BGP, DIV, IF, JOYP, LCDC, LY, LYC, NR10, NR30, NR32, NR41, NR44, NR52, OBP0, OBP1, SC, SCX,
+    SCY, STAT, TAC, TIMA, TMA, WX, WY,
+};
+use hw_constants::{MEM_MAP_SIZE, OAM_END, OAM_START, PostBoot, VRAM_END, VRAM_START};
 use log::info;
 use rkyv::{Archive, Deserialize, Serialize, with::Skip};
 
 #[derive(Archive, Deserialize, Serialize)]
 pub struct AddressBus {
-    pub buffer: Box<[u8; hw_constants::MEM_MAP_SIZE]>,
+    pub buffer: Box<[u8; MEM_MAP_SIZE]>,
     timers: Timers,
     #[rkyv(with = Skip)]
     pub ppu: Ppu,
@@ -27,9 +30,9 @@ impl AddressBus {
         self.mbc.load_rom(rom);
     }
 
-    // TODO: delegate MBC bank switches.
     pub fn read_byte(&self, index: u16) -> u8 {
         match index {
+            // Delegate reads to the PPU.
             VRAM_START..VRAM_END
             | OAM_START..OAM_END
             | LCDC
@@ -43,6 +46,8 @@ impl AddressBus {
             | OBP1
             | WY
             | WX => self.ppu.read_byte(index),
+
+            // TODO: Delegate MBC bank switches.
             _ => self.buffer[index as usize],
         }
     }
@@ -76,7 +81,7 @@ impl AddressBus {
 
             // Certain I/O addresses only use certain bits. Bits which go unused are pulled high.
             // See Appendix B: https://gekkio.fi/files/gb-docs/gbctr.pdf
-            io_regs::JOYP => {
+            JOYP => {
                 // Lower nibble is read-only, only set select bits.
                 // See: https://gbdev.io/pandocs/Joypad_Input.html#ff00--p1joyp-joypad
                 let written = Joyp::from_bits(value);
@@ -87,22 +92,22 @@ impl AddressBus {
 
                 self.update_joypad();
             }
-            io_regs::SC => self.buffer[index as usize] = value | 0b0111_1110,
-            io_regs::TAC => self.buffer[index as usize] = value | 0b1111_1000,
-            io_regs::DIV => self.timers.system_clock = 0,
-            io_regs::IF => self.buffer[index as usize] = value | 0b1110_0000,
+            SC => self.buffer[index as usize] = value | 0b0111_1110,
+            TAC => self.buffer[index as usize] = value | 0b1111_1000,
+            DIV => self.timers.system_clock = 0,
+            IF => self.buffer[index as usize] = value | 0b1110_0000,
 
-            io_regs::NR10 => self.buffer[index as usize] = value | 0b1000_0000,
-            io_regs::NR30 => self.buffer[index as usize] = value | 0b0111_1111,
-            io_regs::NR32 => self.buffer[index as usize] = value | 0b1001_1111,
-            io_regs::NR41 => self.buffer[index as usize] = value | 0b1100_0000,
-            io_regs::NR44 => self.buffer[index as usize] = value | 0b0011_1111,
-            io_regs::NR52 => self.buffer[index as usize] = value | 0b0111_0000,
+            NR10 => self.buffer[index as usize] = value | 0b1000_0000,
+            NR30 => self.buffer[index as usize] = value | 0b0111_1111,
+            NR32 => self.buffer[index as usize] = value | 0b1001_1111,
+            NR41 => self.buffer[index as usize] = value | 0b1100_0000,
+            NR44 => self.buffer[index as usize] = value | 0b0011_1111,
+            NR52 => self.buffer[index as usize] = value | 0b0111_0000,
 
             // Delegate PPU registers to the PPU.
             LCDC => self.ppu.write_byte(index, value),
             // Still needed until I can update interrupts without passing in all memory :(.
-            io_regs::STAT => {
+            STAT => {
                 self.ppu.write_byte(index, value);
 
                 if !self.ppu.disabled {
@@ -140,20 +145,17 @@ impl AddressBus {
         }
         self.half_ticked = false;
 
-        self.timers
-            .update_timer_counter(self.buffer[io_regs::TIMA as usize]);
-        self.timers
-            .update_timer_modulo(self.buffer[io_regs::TMA as usize]);
-        self.timers
-            .update_timer_control(self.buffer[io_regs::TAC as usize]);
+        self.timers.update_timer_counter(self.buffer[TIMA as usize]);
+        self.timers.update_timer_modulo(self.buffer[TMA as usize]);
+        self.timers.update_timer_control(self.buffer[TAC as usize]);
 
         self.timers.increment(1);
 
-        self.buffer[io_regs::DIV as usize] = self.timers.div();
-        self.buffer[io_regs::TIMA as usize] = self.timers.tima();
+        self.buffer[DIV as usize] = self.timers.div();
+        self.buffer[TIMA as usize] = self.timers.tima();
 
         if self.timers.process_interrupt() {
-            self.buffer[io_regs::IF as usize] |= 0b0000_0100;
+            self.buffer[IF as usize] |= 0b0000_0100;
         }
     }
 
@@ -162,25 +164,22 @@ impl AddressBus {
             self.ppu.tick(self.buffer.as_mut_array().unwrap());
         }
 
-        self.timers
-            .update_timer_counter(self.buffer[io_regs::TIMA as usize]);
-        self.timers
-            .update_timer_modulo(self.buffer[io_regs::TMA as usize]);
-        self.timers
-            .update_timer_control(self.buffer[io_regs::TAC as usize]);
+        self.timers.update_timer_counter(self.buffer[TIMA as usize]);
+        self.timers.update_timer_modulo(self.buffer[TMA as usize]);
+        self.timers.update_timer_control(self.buffer[TAC as usize]);
 
         self.timers.increment(m_cycles);
 
-        self.buffer[io_regs::DIV as usize] = self.timers.div();
-        self.buffer[io_regs::TIMA as usize] = self.timers.tima();
+        self.buffer[DIV as usize] = self.timers.div();
+        self.buffer[TIMA as usize] = self.timers.tima();
 
         if self.timers.process_interrupt() {
-            self.buffer[io_regs::IF as usize] |= 0b0000_0100;
+            self.buffer[IF as usize] |= 0b0000_0100;
         }
     }
 
     pub(crate) fn update_joypad(&mut self) {
-        let mut joypad = Joyp::from_bits(self.buffer[io_regs::JOYP as usize]);
+        let mut joypad = Joyp::from_bits(self.buffer[JOYP as usize]);
         if !joypad.select_buttons() {
             joypad.set_start_down(!self.buttons_held.start);
             joypad.set_select_up(!self.buttons_held.select);
@@ -194,17 +193,14 @@ impl AddressBus {
             joypad.set_a_right(!self.buttons_held.right);
         }
         // TODO: Fire the joypad interrupt on a high-to-low change
-        self.buffer[io_regs::JOYP as usize] = joypad.into_bits();
+        self.buffer[JOYP as usize] = joypad.into_bits();
     }
 }
 
 impl Default for AddressBus {
     fn default() -> Self {
         Self {
-            buffer: vec![0; hw_constants::MEM_MAP_SIZE]
-                .into_boxed_slice()
-                .try_into()
-                .unwrap(),
+            buffer: vec![0; MEM_MAP_SIZE].into_boxed_slice().try_into().unwrap(),
             timers: Timers::default(),
             ppu: Ppu::default(),
             mbc: Mbc::default(),
