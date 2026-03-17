ad61ffaacb497c0327e54c666d3a7ab669313669
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-03-17 15:36:05 +0000

    Pass all Mooneye timer tests sans the TIMA ones.

--- sm83-interp/tests/mooneye_timer.rs
+++ sm83-interp/tests/mooneye_timer.rs
@@ -7,3 +7,57 @@ fn test_div_write() {
     let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/div_write.gb"));
     assert_eq!(bcdehl, FIBONACCI);
 }
+
+#[test]
+fn test_rapid_toggle() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/rapid_toggle.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
+
+#[test]
+fn test_tim00_div_trigger() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/tim00_div_trigger.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
+
+#[test]
+fn test_tim00() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/tim00.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
+
+#[test]
+fn test_tim01_div_trigger() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/tim01_div_trigger.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
+
+#[test]
+fn test_tim01() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/tim01.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
+
+#[test]
+fn test_tim10_div_trigger() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/tim10_div_trigger.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
+
+#[test]
+fn test_tim10() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/tim10.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
+
+#[test]
+fn test_tim11_div_trigger() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/tim11_div_trigger.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
+
+#[test]
+fn test_tim11() {
+    let bcdehl = run_mooneye_test(include_bytes!("roms/mooneye/timer/tim11.gb"));
+    assert_eq!(bcdehl, FIBONACCI);
+}
