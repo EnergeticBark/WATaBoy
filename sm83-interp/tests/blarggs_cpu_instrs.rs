159dc9495bac3edbf091a7d93b88b868aa50410a
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-03-19 14:49:43 +0000

    Create each BlarggTest inline.

--- sm83-interp/tests/blarggs_cpu_instrs.rs
+++ sm83-interp/tests/blarggs_cpu_instrs.rs
@@ -2,114 +2,102 @@ pub mod common;
 
 use common::blarggs::{BlarggTest, run_blargg_test};
 
-const SPECIAL_01: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/01-special.gb"),
-    final_pc: 0xC7D2,
-};
-const INTERRUPTS_02: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/02-interrupts.gb"),
-    final_pc: 0xC7F4,
-};
-const OP_SP_HL_03: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/03-op sp,hl.gb"),
-    final_pc: 0xCB44,
-};
-const OP_R_IMM_04: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/04-op r,imm.gb"),
-    final_pc: 0xCB35,
-};
-const OP_RP_05: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/05-op rp.gb"),
-    final_pc: 0xCB31,
-};
-const LD_R_R_06: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/06-ld r,r.gb"),
-    final_pc: 0xCC5F,
-};
-const JR_JP_CALL_RET_RST_07: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/07-jr,jp,call,ret,rst.gb"),
-    final_pc: 0xCBB0,
-};
-const MISC_INSTRS_08: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/08-misc instrs.gb"),
-    final_pc: 0xCB91,
-};
-const OP_R_R_09: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/09-op r,r.gb"),
-    final_pc: 0xCE67,
-};
-const BIT_OPS_10: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/10-bit ops.gb"),
-    final_pc: 0xCF58,
-};
-const OP_A_HL_11: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/cpu_instrs/11-op a,(hl).gb"),
-    final_pc: 0xCC62,
-};
-
 #[test]
 fn test_01_special() {
-    let lines = run_blargg_test(&SPECIAL_01);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/01-special.gb"),
+        final_pc: 0xC7D2,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_02_interrupts() {
-    let lines = run_blargg_test(&INTERRUPTS_02);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/02-interrupts.gb"),
+        final_pc: 0xC7F4,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_03_op_sp_hl() {
-    let lines = run_blargg_test(&OP_SP_HL_03);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/03-op sp,hl.gb"),
+        final_pc: 0xCB44,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_04_op_r_imm() {
-    let lines = run_blargg_test(&OP_R_IMM_04);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/04-op r,imm.gb"),
+        final_pc: 0xCB35,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_05_op_rp() {
-    let lines = run_blargg_test(&OP_RP_05);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/05-op rp.gb"),
+        final_pc: 0xCB31,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_06_ld_r_r() {
-    let lines = run_blargg_test(&LD_R_R_06);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/06-ld r,r.gb"),
+        final_pc: 0xCC5F,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_07_jr_jp_call_ret_rst() {
-    let lines = run_blargg_test(&JR_JP_CALL_RET_RST_07);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/07-jr,jp,call,ret,rst.gb"),
+        final_pc: 0xCBB0,
+    });
     // This test's name is so long that "Passed" ends up on line 4 :)
     assert!(lines[4].starts_with("Passed"));
 }
 
 #[test]
 fn test_08_misc_instrs() {
-    let lines = run_blargg_test(&MISC_INSTRS_08);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/08-misc instrs.gb"),
+        final_pc: 0xCB91,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_09_op_r_r() {
-    let lines = run_blargg_test(&OP_R_R_09);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/09-op r,r.gb"),
+        final_pc: 0xCE67,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_10_bit_ops() {
-    let lines = run_blargg_test(&BIT_OPS_10);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/10-bit ops.gb"),
+        final_pc: 0xCF58,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_11_op_a_hl() {
-    let lines = run_blargg_test(&OP_A_HL_11);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/cpu_instrs/11-op a,(hl).gb"),
+        final_pc: 0xCC62,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
--- sm83-interp/tests/blarggs_instr_timing.rs
+++ sm83-interp/tests/blarggs_instr_timing.rs
@@ -2,13 +2,11 @@ pub mod common;
 
 use common::blarggs::{BlarggTest, run_blargg_test};
 
-const INSTR_TIMING: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/instr_timing/instr_timing.gb"),
-    final_pc: 0xC8B0,
-};
-
 #[test]
 fn test_instr_timing() {
-    let lines = run_blargg_test(&INSTR_TIMING);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/instr_timing/instr_timing.gb"),
+        final_pc: 0xC8B0,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
