e62150f08e9e97f300e9935e8eccea18e8cb07bb
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-03-19 14:49:43 +0000

    Create each BlarggTest inline.

--- sm83-interp/tests/blarggs_mem_timing_2.rs
+++ sm83-interp/tests/blarggs_mem_timing_2.rs
@@ -2,35 +2,29 @@ pub mod common;
 
 use common::blarggs::{BlarggTest, run_blargg_test};
 
-const READ_TIMING_01: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/01-read_timing.gb"),
-    final_pc: 0xC8BE,
-};
-
-const WRITE_TIMING_02: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/02-write_timing.gb"),
-    final_pc: 0xC87C,
-};
-
-const MODIFY_TIMING_03: BlarggTest = BlarggTest {
-    rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/03-modify_timing.gb"),
-    final_pc: 0xC8E4,
-};
-
 #[test]
 fn test_01_read_timing() {
-    let lines = run_blargg_test(&READ_TIMING_01);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/01-read_timing.gb"),
+        final_pc: 0xC8BE,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_02_read_timing() {
-    let lines = run_blargg_test(&WRITE_TIMING_02);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/02-write_timing.gb"),
+        final_pc: 0xC87C,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
 
 #[test]
 fn test_03_modify_timing() {
-    let lines = run_blargg_test(&MODIFY_TIMING_03);
+    let lines = run_blargg_test(&BlarggTest {
+        rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/03-modify_timing.gb"),
+        final_pc: 0xC8E4,
+    });
     assert!(lines[3].starts_with("Passed"));
 }
