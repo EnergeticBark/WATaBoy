926df62340bbdba40c6061bd15b3b698085e6154
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-04-29 13:23:30 +0100

    Remove duplicated test ROMs for Bun tests.

--- sm83-interp/tests/blarggs_mem_timing_2.rs
+++ sm83-interp/tests/blarggs_mem_timing_2.rs
@@ -5,7 +5,7 @@ use common::blarggs::{BlarggTest, run_blargg_test};
 #[test]
 fn test_01_read_timing() {
     let lines = run_blargg_test(&BlarggTest {
-        rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/01-read_timing.gb"),
+        rom: include_bytes!("roms/blarggs/mem_timing_2/01-read_timing.gb"),
         final_pc: 0xC8BE,
     });
     assert!(lines[3].starts_with("Passed"));
@@ -14,7 +14,7 @@ fn test_01_read_timing() {
 #[test]
 fn test_02_read_timing() {
     let lines = run_blargg_test(&BlarggTest {
-        rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/02-write_timing.gb"),
+        rom: include_bytes!("roms/blarggs/mem_timing_2/02-write_timing.gb"),
         final_pc: 0xC87C,
     });
     assert!(lines[3].starts_with("Passed"));
@@ -23,7 +23,7 @@ fn test_02_read_timing() {
 #[test]
 fn test_03_modify_timing() {
     let lines = run_blargg_test(&BlarggTest {
-        rom: include_bytes!("roms/blarggs/mem_timing_2/rom_singles/03-modify_timing.gb"),
+        rom: include_bytes!("roms/blarggs/mem_timing_2/03-modify_timing.gb"),
         final_pc: 0xC8E4,
     });
     assert!(lines[3].starts_with("Passed"));
