0f30df8ad9677467773c3700e3bea86d40f8a632
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-05-10 11:35:34 +0100

    Better comments and function names. Clean up JitRuntime.

--- jit/src/lib.rs
+++ jit/src/lib.rs
@@ -11,16 +11,27 @@ pub mod test_roms;
 static TALC: talc::wasm::WasmDynamicTalc = talc::wasm::new_wasm_dynamic_allocator();
 
 unsafe extern "C" {
+    fn link_new_module_glue(buffer: *const u8, len: usize) -> i32;
+
     fn console_log_glue(buffer: *const u8, len: usize);
 }
 
+/// Compile and instantiate a new Wasm module using the bytecode in `buffer`, then add its exported function to *this* module's function table.
+/// ## Returns
+/// The index of the newly linked function.
+fn link_new_module(buffer: &[u8]) -> i32 {
+    unsafe { link_new_module_glue(buffer.as_ptr(), buffer.len()) }
+}
+
+/// Log the UTF-8 contents of `buffer` to the embedder's console.
 /// This function is expensive to call, it should have no usages in the codebase unless it's actively being used for debugging.
-pub fn console_log(message: &str) {
+fn console_log(message: &str) {
     unsafe {
         console_log_glue(message.as_ptr(), message.len());
     }
 }
 
+/// Indirectly call the function at `index` in this module's function table.
 fn call_indirect(index: i32) {
     unsafe {
         asm!(
