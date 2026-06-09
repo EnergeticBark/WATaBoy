86de11aaab3ea7260b6e3961c8ad60f650e17b08
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-06-09 12:50:28 +0100

    Implement MBC5’s 9-bit bank switching.

--- interpreter/src/mbc/mbc5.rs
+++ interpreter/src/mbc/mbc5.rs
@@ -4,57 +4,60 @@ use rkyv::{Archive, Deserialize, Serialize};
 
 use hw_constants::{ROM_BANK_0_END, SRAM_END, SRAM_START};
 
-const RAM_SIZE_ADDR: usize = 0x0149;
+const SRAM_SIZE_ADDR: usize = 0x0149;
 
-const RAM_BANK_SIZE: usize = 0x2000;
+const ROM_BANK_SIZE: usize = 0x4000;
+const SRAM_BANK_SIZE: usize = 0x2000;
 
-const MBC1_ROM_BANK_MASK: u8 = 0b0001_1111;
-const MBC1_SRAM_BANK_MASK: u8 = 0b0000_0011;
+const MBC5_SRAM_BANK_MASK: u8 = 0b0000_1111;
 
 #[derive(Archive, Deserialize, Serialize)]
 pub(crate) struct Mbc5 {
     ram_enabled: bool,
     pub rom: Vec<u8>,
-    ext_ram: Vec<u8>,
-    pub current_rom_bank: u8,
+    sram: Vec<u8>,
+    pub current_rom_bank: u16,
     current_rom_bank_start: usize,
     current_ram_bank: u8,
-    banking_mode: bool,
 }
 
 impl Mbc5 {
     pub fn from_rom(rom: &[u8]) -> Self {
-        let ext_ram = match rom[RAM_SIZE_ADDR] {
-            2 => vec![0; RAM_BANK_SIZE],
-            3 => vec![0; RAM_BANK_SIZE * 4],
+        let sram = match rom[SRAM_SIZE_ADDR] {
+            2 => vec![0; SRAM_BANK_SIZE],
+            3 => vec![0; SRAM_BANK_SIZE * 4],
             _ => vec![],
         };
 
         Self {
             rom: rom.to_vec(),
-            ext_ram,
+            sram,
             ..Default::default()
         }
     }
 
     fn ram_size(&self) -> u8 {
-        self.rom[RAM_SIZE_ADDR]
+        self.rom[SRAM_SIZE_ADDR]
     }
 
-    fn update_rom_bank(&mut self, bank_number: u8) {
-        let mut bank_number = bank_number & MBC1_ROM_BANK_MASK;
-        if bank_number == 0 {
-            bank_number = 1;
-        }
+    fn update_rom_bank(&mut self, mut bank_number: u16) {
+        // ROM bank 0 is allowed to be mapped.
+
+        // Constrain the bank number to the number of rom banks.
+        let num_rom_banks = u16::try_from(self.rom.len() / ROM_BANK_SIZE).unwrap();
+        bank_number %= num_rom_banks;
+
+        println!("Switching to ROM bank #{bank_number}");
+
         #[cfg(feature = "mbc-logging")]
         info!(target: "mbc_events", "Switching to ROM bank #{bank_number}");
 
         self.current_rom_bank = bank_number;
-        self.current_rom_bank_start = 0x4000 * (bank_number as usize - 1);
+        self.current_rom_bank_start = 0x4000 * bank_number as usize;
     }
 
     fn update_ram_bank(&mut self, bank_number: u8) {
-        let mut bank_number = bank_number & MBC1_SRAM_BANK_MASK;
+        let mut bank_number = bank_number & MBC5_SRAM_BANK_MASK;
         #[cfg(feature = "mbc-logging")]
         info!(target: "mbc_events", "Switching to SRAM bank #{bank_number}");
 
@@ -67,30 +70,16 @@ impl Mbc5 {
         self.current_ram_bank = bank_number;
     }
 
-    // TODO: implement 1 MiB ROM support.
-    // See: https://gbdev.io/pandocs/MBC1.html#40005fff--ram-bank-number--or--upper-bits-of-rom-bank-number-write-only
-    fn nth_ram_bank(&self, bank_number: u8) -> &[u8; RAM_BANK_SIZE] {
-        let start_addr = RAM_BANK_SIZE * bank_number as usize;
-        let end_addr = start_addr + RAM_BANK_SIZE;
-        self.ext_ram[start_addr..end_addr].try_into().unwrap()
+    fn nth_ram_bank(&self, bank_number: u8) -> &[u8; SRAM_BANK_SIZE] {
+        let start_addr = SRAM_BANK_SIZE * bank_number as usize;
+        let end_addr = start_addr + SRAM_BANK_SIZE;
+        self.sram[start_addr..end_addr].try_into().unwrap()
     }
 
-    // TODO: implement 1 MiB ROM support.
-    // See: https://gbdev.io/pandocs/MBC1.html#40005fff--ram-bank-number--or--upper-bits-of-rom-bank-number-write-only
-    fn nth_ram_bank_mut(&mut self, bank_number: u8) -> &mut [u8; RAM_BANK_SIZE] {
-        let start_addr = RAM_BANK_SIZE * bank_number as usize;
-        let end_addr = start_addr + RAM_BANK_SIZE;
-        (&mut self.ext_ram[start_addr..end_addr])
-            .try_into()
-            .unwrap()
-    }
-
-    fn set_banking_mode(&mut self, banking_mode: u8) {
-        let banking_mode = banking_mode & 1 == 1;
-        #[cfg(feature = "mbc-logging")]
-        info!(target: "mbc_events", "Switching to banking mode {banking_mode}");
-
-        self.banking_mode = banking_mode;
+    fn nth_ram_bank_mut(&mut self, bank_number: u8) -> &mut [u8; SRAM_BANK_SIZE] {
+        let start_addr = SRAM_BANK_SIZE * bank_number as usize;
+        let end_addr = start_addr + SRAM_BANK_SIZE;
+        (&mut self.sram[start_addr..end_addr]).try_into().unwrap()
     }
 
     pub fn read_byte(&self, index: u16) -> u8 {
@@ -107,7 +96,7 @@ impl Mbc5 {
                 }
             }
             _ => {
-                let translated = self.current_rom_bank_start + index as usize;
+                let translated = self.current_rom_bank_start + (index - 0x4000) as usize;
                 unsafe { *self.rom.get_unchecked(translated) }
             }
         }
@@ -128,27 +117,34 @@ impl Mbc5 {
                 info!(target: "mbc_events", "Disabling SRAM...");
                 self.ram_enabled = false;
             }
-            // MBC1: ROM Bank Number
-            0x2000..0x4000 => {
-                #[cfg(feature = "mbc-logging")]
-                info!(target: "mbc_events", "Switching ROM bank using value: {value}");
+            // MBC5: Lower 8 Bits of ROM Bank Number
+            0x2000..0x3000 => {
+                let prev_8th_bit = self.current_rom_bank & (1 << 8);
+                let bank_number = prev_8th_bit | u16::from(value);
 
-                self.update_rom_bank(value);
+                #[cfg(feature = "mbc-logging")]
+                info!(target: "mbc_events", "Switching ROM bank using value: {bank_number}");
+
+                self.update_rom_bank(bank_number);
+            }
+            // MBC5: 8th Bit of ROM Bank Number
+            0x3000..0x4000 => {
+                let prev_lo_byte = self.current_rom_bank & 0xFF;
+                let bank_number = prev_lo_byte | ((u16::from(value) & 1) << 8);
+
+                #[cfg(feature = "mbc-logging")]
+                info!(target: "mbc_events", "Switching ROM bank using value: {bank_number}");
+
+                self.update_rom_bank(bank_number);
             }
             // MBC1: RAM Bank Number or Upper Bits of ROM bank number
             0x4000..0x6000 => {
                 #[cfg(feature = "mbc-logging")]
                 info!(target: "mbc_events", "Switching SRAM bank using value: {value}");
 
-                if self.banking_mode {
-                    self.update_ram_bank(value);
-                } else {
-                    #[cfg(feature = "mbc-logging")]
-                    info!(target: "mbc_events", "Actually no, we're in simple mode!");
-                }
+                self.update_ram_bank(value);
             }
-            // MBC1: Banking Mode Select
-            0x6000..0x8000 => self.set_banking_mode(value),
+            0x6000..0x8000 => (),
 
             // MBC1: SRAM
             SRAM_START..SRAM_END => {
@@ -169,11 +165,10 @@ impl Default for Mbc5 {
         Self {
             ram_enabled: false,
             rom: Vec::new(),
-            ext_ram: Vec::new(),
+            sram: Vec::new(),
             current_rom_bank: 1,
-            current_rom_bank_start: 0,
+            current_rom_bank_start: 0x4000,
             current_ram_bank: 0,
-            banking_mode: false,
         }
     }
 }
