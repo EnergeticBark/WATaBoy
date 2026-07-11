ce35bbeafd42956ceec71c61b18eac8d45519d03
EnergeticBark <33643461+EnergeticBark@users.noreply.github.com>
2026-07-11 19:40:12 +0100

    Add `intr_timing.gb` and `daa.gb` to JIT tests

--- jit/js/__tests__/mooneye.test.js
+++ jit/js/__tests__/mooneye.test.js
@@ -5,16 +5,17 @@ import { Runtime } from "../runtime.js";
 // The first param is the rom name, the second param is the line in the tile map where "Passed" will appear. 
 const roms = [
 	"add_sp_e_timing.gb",
+	// TODO: Implement boot ROM skipping so I can pass this without distributing the original mgb boot ROM.
+	//"boot_div-dmgABCmgb.gb",
+	//"boot_hwio-dmgABCmgb.gb",
+	"boot_regs-mgb.gb",
 	"call_cc_timing.gb",
 	"call_cc_timing2.gb",
 	"call_timing.gb",
 	"call_timing2.gb",
 	"di_timing-GS.gb",
 	"div_timing.gb",
-	// TODO: Implement boot ROM skipping so I can pass this without distributing the original mgb boot ROM.
-	//"boot_div-dmgABCmgb.gb",
-	//"boot_hwio-dmgABCmgb.gb",
-	"boot_regs-mgb.gb",
+	"intr_timing.gb",
 	"oam_dma_restart.gb",
 	"oam_dma_start.gb",
 	"oam_dma_timing.gb"
@@ -26,7 +27,7 @@ test.each(roms)("%p", async (romName) => {
 	await runtime.init(wasmSource);
 	const rom = await Bun.file(testROMPath + "mooneye/" + romName).bytes();
 	runtime.loadRom(rom);
-	
+
 	let passed = runtime.runMooneyeTest();
 	expect(passed).toBeTruthy();
 });
